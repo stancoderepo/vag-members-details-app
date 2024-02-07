d2e46a3e23ff3676e5a5e10fa73ad56cc8113f13
stancoderepo <157030551+stancoderepo@users.noreply.github.com>
2024-02-07 10:40:43 +0000

    Draft format of Member application

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-  return "<p>Hello, World!</p>"
+  return render_template('application.html')
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080, debug=True)
